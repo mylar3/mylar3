2c6b80ca5d13b359db15fecbb4f266efd01ed0db
evilhero <evilhero@gmail.com>
2012-09-18 09:47:01 -0400

    added mylar.init.d for autostartup on ubuntu

---
+++ mylar.init.d
@@ -0,0 +1,65 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:          mylar
+# Required-Start:    $local_fs $network $remote_fs
+# Required-Stop:     $local_fs $network $remote_fs
+# Should-Start:      $NetworkManager
+# Should-Stop:       $NetworkManager
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts instance of mylar
+# Description:       starts instance of mylar using start-stop-daemon
+### END INIT INFO
+
+############### EDIT ME ##################
+# path to app
+APP_PATH=
+
+# path to python bin
+DAEMON=/usr/bin/python
+
+# startup args
+DAEMON_OPTS=" Mylar.py -q"
+
+# script name
+NAME=mylar
+
+# app name
+DESC=mylar
+
+# user
+RUN_AS=
+
+PID_FILE=/var/run/mylar.pid
+
+############### END EDIT ME ##################
+
+test -x $DAEMON || exit 0
+
+set -e
+
+case "$1" in
+  start)
+        echo "Starting $DESC"
+        start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $PID_FILE  --make-pidfile --exec $DAEMON -- $DAEMON_OPTS
+        ;;
+  stop)
+        echo "Stopping $DESC"
+        start-stop-daemon --stop --pidfile $PID_FILE
+        ;;
+
+  restart|force-reload)
+        echo "Restarting $DESC"
+        start-stop-daemon --stop --pidfile $PID_FILE
+        sleep 15
+        start-stop-daemon -d $APP_PATH -c $RUN_AS --start --background --pidfile $PID_FILE  --make-pidfile --exec $DAEMON -- $DAEMON_OPTS
+        ;;
+  *)
+        N=/etc/init.d/$NAME
+        echo "Usage: $N {start|stop|restart|force-reload}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
