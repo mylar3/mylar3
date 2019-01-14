313f60195d4c35e2252809ce457b79ffcf95502a
evilhero <evilhero@gmail.com>
2019-01-14 09:54:23 -0500

    FIX: updated forked-CT version to 1.25.2

--- lib/comictaggerlib/ctversion.py
+++ lib/comictaggerlib/ctversion.py
@@ -1,5 +1,5 @@
 # This file should contain only these comments, and the line below.
 # Used by packaging makefiles and app
-version = "1.25.1"
+version = "1.25.2"
 fork="ninjas.walk.alone"
 fork_tag="SHURIKEN"
