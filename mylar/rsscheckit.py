4a5c8f8d9205ff178ff368f40b8b557e8eb84732
Barbeque Sauce <saucebarbeque@hotmail.com>
2020-03-18 11:26:00 -0400

    FIX?: try/except around 32p rss feedinfo
    explains #191 better

--- mylar/rsscheckit.py
+++ mylar/rsscheckit.py
@@ -84,7 +84,11 @@ class tehMain():
                             else:
                                 feeds = mylar.FEEDINFO_32P
                             if continue_search is True:
-                                logger.info('feedinfo: %s' % feedinfo)
+                                try:
+                                    logger.info('feedinfo: %s' % feedinfo)
+                                except Exception as e:
+                                    logger.warn('[32P-RSSFEED] Unable to retrieve RSS properly: %s' % e)
+                                    continue
                                 if feedinfo is None or all([len(feedinfo) == 0 , feedinfo['status'] is False]):
                                     logger.error('[RSS-FEEDS] Unable to retrieve any information from 32P for RSS Feeds. Skipping for now.')
                                 else:
