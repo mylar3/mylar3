d14353a24c0997cc095fd5206bf509625b760aef
Barbeque Sauce <saucebarbeque@hotmail.com>
2020-06-23 19:23:42 -0400

    FIX: readlist instead of readinglist
    Yo dawg, I heard you liked readinglists so I gotchu a readlist for your readinglist

--- mylar/opds.py
+++ mylar/opds.py
@@ -87,7 +87,7 @@ class OPDS(object):
                 if self.issue_id:
                     try:
                         logger.fdebug('OPDS is attempting to markasRead filename %s aka issue_id %s' % (self.filename, self.issue_id))
-                        readinglist.Readinglist(IssueID=self.issue_id).markasRead()
+                        readlist.Readinglist(IssueID=self.issue_id).markasRead()
                     except:
                         logger.fdebug('No reading list found to update.')
                 return serve_download(path=self.file, name=self.filename)
