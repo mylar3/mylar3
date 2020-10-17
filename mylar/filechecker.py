c1099a9fa5ae0b48a174b1b821a12d955dee0034
evilhero <909424+evilhero@users.noreply.github.com>
2020-10-17 13:24:56 -0400

    FIX:(#496) when parsing filenames (search / post-processing / refresh) would incorrectly parse some items in the filename as if they belonged to an issue exception listing

--- mylar/filechecker.py
+++ mylar/filechecker.py
@@ -480,7 +480,7 @@ class FileChecker(object):
 
             #this handles the exceptions list in the match for alpha-numerics
             test_exception = ''.join([i for i in sf if not i.isdigit()])
-            if any(ext in test_exception.upper() for ext in mylar.ISSUE_EXCEPTIONS):
+            if any(ext == test_exception.upper() for ext in mylar.ISSUE_EXCEPTIONS):
                 logger.fdebug('Exception match: %s' % test_exception)
                 if lastissue_label is not None:
                     if lastissue_position == (split_file.index(sf) -1):
