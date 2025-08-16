1640cf1513d86c31ac5ec0acaa70f42c22df7878
falo2k <github@falo.co.uk>
2025-08-16 13:39:53 +0100

    IMP: Ability to add an instance name to the browser title

--- mylar/config.py
+++ mylar/config.py
@@ -147,6 +147,7 @@ _CONFIG_DEFINITIONS = OrderedDict({
     'LOGIN_TIMEOUT': (int, 'Interface', 43800),
     'ALPHAINDEX': (bool, 'Interface', True),
     'CHERRYPY_LOGGING': (bool, 'Interface', False),
+    'INSTANCE_NAME': (str, 'Interface', None),
 
     'API_ENABLED' : (bool, 'API', False),
     'API_KEY' : (str, 'API', None),
