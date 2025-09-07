e1723fa9687d163acaf71a9785ce579dcb8ba68f
falo <1392563+falo2k@users.noreply.github.com>
2025-09-07 18:49:45 +0100

    FIX: Better handling of bad CBL list data (issue not in volume)

--- mylar/webserve.py
+++ mylar/webserve.py
@@ -9683,6 +9683,7 @@ class WebInterface(object):
         issuesonly = True if issuesonly == 'true' else False
 
         results = []
+        warnings = set()
 
         try:
             books = cblRoot.findall(".//Book")
@@ -9741,6 +9742,10 @@ class WebInterface(object):
                         else:
                             missing_issue_count += 1
                             action_text = 'Mark issue as Wanted'
+                    else:
+                        action_text = f'Warning: Could not find issue {cvIssueID} in volume {cvSeriesID}.'
+                        missing_issue_count += 1
+                        warnings.add('Volumes in Mylar do not recognise some Issue IDs.  This may be due to a list error.')
                 else:
                     iss_exists = False
                     iss_status = 'Missing'
@@ -9758,9 +9763,12 @@ class WebInterface(object):
             return json.dumps({'status': 'error', 'message' : f'Error processing CBL file.  Unhandled Exception: {str(e)}'})
         
         if newvol_count > 200:
-            warning_text = 'Attempting to add more than 200 series may cause problems due to CV API limits.  Consider breaking up this list into smaller parts.'
+            warnings.add('Attempting to add more than 200 series may cause problems due to CV API limits.  Consider breaking up this list into smaller parts.')
         elif newvol_count > 100:
-            warning_text = 'Attempting to add a lot of new series.  Consider your CV API limits before triggering the import.'
+            warnings.add('Attempting to add a lot of new series.  Consider your CV API limits before triggering the import.')
+
+        if len(warnings) > 0:
+            warning_text = '<br >'.join(warnings)
         else:
             warning_text = ''
 
@@ -9787,7 +9795,7 @@ class WebInterface(object):
 
         logger.info(f'Processing CBL File {cblFile.filename} to set up volumes and wanted issues')
         warning_text = ''
-        warnings = {}
+        warnings = set()
 
         try:
             cblXML = ET.parse(cblFile.file)
