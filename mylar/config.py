aceaabdc13ab845893f638021408aca6e9b075a6
Jason Ramboz <1568691+jramboz@users.noreply.github.com>
2026-03-19 21:02:13 -0400

    Change default for curl_cffi to False

--- mylar/config.py
+++ mylar/config.py
@@ -380,7 +380,7 @@ _CONFIG_DEFINITIONS = OrderedDict({
     'JD2_ENABLE': (bool, 'DDL', False),
     'JD2_DEST_DIR': (str, 'DDL', None),
     'JD2_URL': (str, 'DDL', None),
-    'USE_CURL_CFFI': (bool, 'DDL', True),
+    'USE_CURL_CFFI': (bool, 'DDL', False),
 
     'ENABLE_AIRDCPP': (bool, 'DCPP', False),
     'AIRDCPP_HOST': (str, 'DCPP', ""),
